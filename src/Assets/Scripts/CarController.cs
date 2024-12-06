f51b9b1abc2972dc47535739110826dd7198cfa2
ErfanMelon <ErfanMelon@outlook.com>
2024-12-06 15:34:38 +0330

    feat(car): Add engine sound

--- src/Assets/Scripts/CarController.cs
+++ src/Assets/Scripts/CarController.cs
@@ -4,11 +4,33 @@ public class CarController : MonoBehaviour
 {
     public float speed = 10f;
     public float turnSpeed = 50f;
+    public AudioSource gasSound;
 
     void Update()
     {
+        float move = 0f;
+
+        // Check for movement and play sound
+        if (Input.GetKey(KeyCode.W))
+        {
+            move = speed * Time.deltaTime;
+
+            // Play gas sound if it's not already playing
+            if (!gasSound.isPlaying)
+            {
+                gasSound.Play();
+            }
+        }
+        else
+        {
+            // Stop the gas sound when W is released
+            if (gasSound.isPlaying)
+            {
+                gasSound.Stop();
+            }
+        }
         // Move forward and backward
-        float move = Input.GetAxis("Vertical") * speed * Time.deltaTime * -1;
+        move = Input.GetAxis("Vertical") * speed * Time.deltaTime * -1;
         transform.Translate(0, 0, move);
 
         // Turn left and right
